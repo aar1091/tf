12b84a9fe370ac4e4ef241c2bffa6aa6bd5dd05b
aar1091 <106763996+aar1091@users.noreply.github.com>
2025-05-27 15:14:35 -0400

    test11

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,15 +30,7 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
-}
-
-resource "azurerm_public_ip" "public_ip" {
-  name                = "public-ip-github"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
